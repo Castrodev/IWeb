e55837c87d8051f2085e040db809eba2a28306c2
MarlonF1 <marlon.fdez77@gmail.com>
2018-03-21 15:38:25 -0600

    Subiendo proyecto :100:

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-03-21 01:23:27 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-03-21T01:23:27+00:00	INFO ::1	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
